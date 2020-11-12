3a7a711ef12fa28a89f83b55b4b69a3f8faeb337
TSecret-GitHub <70588156+TSecret-GitHub@users.noreply.github.com>
2020-11-12 16:42:54 +0200

    Некоторое обновление
    Крайне мелкое

--- 1_TSP.py
+++ 1_TSP.py
@@ -14,56 +14,66 @@ def mas(event):
         my_file = open("MLP.txt", "a")
         my_file.write("Метка: " +  met.get()  + " Логин: " + log.get()  + " Пароль: " + pas.get() + '\n')
         my_file.close()
-        master = Toplevel(root)
-        met = StringVar()
-        log = StringVar()
-        pas = StringVar()
-        text_met = Label(master, text="Введи название пароля (метка должна быть уникальной!)")
-        text_met.pack()
-        ent_met = Entry(master, textvariable=met)
-        ent_met.pack()
-        text_log = Label(master, text="Введи логин")
-        text_log.pack()
-        ent_log = Entry(master, textvariable=log)
-        ent_log.pack()
-        text_pas = Label(master, text="Введи пароль")
-        text_pas.pack()
-        ent_pas = Entry(master, textvariable=pas)
-        ent_pas.pack()
-        but = Button(master, text="Создать!")
-        but.bind("<Button-1>", edit)
-        but.pack()
-
+    master = Toplevel(root)
+    master.title("TSP/master - Мастер создания паролей, есть недоработки, но, мы их исправим!")
+    met = StringVar()
+    log = StringVar()
+    pas = StringVar()
+    text_met = Label(master, text="Введи название пароля (метка должна быть уникальной!)")
+    text_met.pack()
+    ent_met = Entry(master, textvariable=met)
+    ent_met.pack()
+    text_log = Label(master, text="Введи логин")
+    text_log.pack()
+    ent_log = Entry(master, textvariable=log)
+    ent_log.pack()
+    text_pas = Label(master, text="Введи пароль")
+    text_pas.pack()
+    ent_pas = Entry(master, textvariable=pas)
+    ent_pas.pack()
+    but = Button(master, text="Создать!")
+    but.bind("<Button-1>", edit)
+    but.pack()
+    
 def search(event):
-     def go(event):
-         word = s.get()
-         filename = 'MLP.txt'
-         with open(filename) as f:
-             test = print(word if word in f.read() else messagebox.showerror('Error', 'Извини, но я ничего не нашел \nВозможно ты не ввел имя метки…'))
-             test = test.str().replace('', '')
-         lookup = s.get()
-         with open(filename) as myFile:
-             for num, line in enumerate(myFile, 0):
-                 if lookup in line:
-                     num = num
-         with open('MLP.txt') as f:
-             for index, line in enumerate(f):
-                 if index == num:
-                     a = line
-                     break
-             f = open('MLP.txt')
-             line = f.readlines()
-             test = line[num]
+    def go(event):
+        word = s.get()
+        filename = 'MLP.txt'
+        with open(filename) as f:
+            test = word if word in f.read() else messagebox.showerror('Error', 'Извини, но я ничего не нашел \nВозможно ты не ввел имя метки…')
+            print(test)
+            test = test.replace('', '')
+        lookup = s.get()
+        with open(filename) as myFile:
+            for num, line in enumerate(myFile, 0):
+                if lookup in line:
+                    num = num
+        with open('MLP.txt') as f:
+            for index, line in enumerate(f):
+                if index == num:
+                    a = line
+                    break
+            f = open('MLP.txt')
+            line = f.readlines()
+            test = line[num]
+            num -= 2
+            print(num)
+            print(line[num])
+            result = Toplevel(root)
+            text = Label(result, text='Ваш результат: {}'.format(line[num]))
+            text.pack()
 
-     s = StringVar()
-     search = Toplevel(root)
-     text = Label(search, text="Введи метку")
-     text.pack()
-     ent = Entry(search, textvariable=s)
-     ent.pack()
-     but = Button(search, text="Искать")
-     but.bind("<Button-1>", go)
-     but.pack()
+    s = StringVar()
+    search = Toplevel(root)
+    search.title("TSP/search - Поиск достоиный Вас!")
+    text = Label(search, text="Введи метку")
+    text.pack()
+    ent = Entry(search, textvariable=s)
+    ent.pack()
+    but = Button(search, text="Искать")
+    but.bind("<Button-1>", go)
+    but.pack()
+    
 
 def settings(event):
     def BS(event):
@@ -77,6 +87,7 @@ def settings(event):
         but.bind("<Button-1>", S)
         but.pack()
     Set = Toplevel(root)
+    Set.title("TSP/settings - Настройки которые НЕ работают")
     but_set = Button(Set, text="Директория паролей")
     but_set.place(relx=.5, rely=.1, anchor="c", height=30, width=130, bordermode=OUTSIDE)
     but_set.bind("<Button-1>", BS)
@@ -84,6 +95,7 @@ def settings(event):
 
 root = Tk()
 root.geometry("370x250")
+root.title("TSP - Сохрани все свои пароли!")
 
 text = Label(text="Добро пожаловать!")
 text.place(relx=.4, rely=.1, anchor="c", height=30, width=130, bordermode=OUTSIDE)
